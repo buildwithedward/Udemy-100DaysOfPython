2c107334b8595da1b6114456a8f28d4b2541655a
edwardcodes <edpraveen1@gmail.com>
2021-05-13 00:08:59 +0530

    modified

--- Day-17/main.py
+++ Day-17/main.py
@@ -7,7 +7,7 @@ question_bank = []
 for question in question_data:
     question_text = question['text']
     question_answer = question['answer']
-    new_question = Question(ques=question_text, answer=question_answer)
+    new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
