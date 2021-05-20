31271d0274f266293f566c3fd2b93ae942afacd7
edwardcodes <edpraveen1@gmail.com>
2021-05-20 23:33:45 +0530

    new

--- Day-18/random-walk.py
+++ Day-18/random-walk.py
@@ -4,5 +4,22 @@ import random
 tim = t.Turtle()
 
 ########### Challenge 4 - Random Walk ########
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
-           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+t.colormode(255)  # setting colormode from 1 to 255
+
+
+def generate_random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+directions = [0, 90, 180, 270]
+tim.pensize(10)  # setting the width of pensize
+tim.speed(10)  # setting the speed as fast - 10. for fastest - 0
+
+for _ in range(200):
+    tim.color(generate_random_color())
+    tim.forward(45)
+    # Set the orientation of the turtle to to_angle
+    tim.setheading(random.choice(directions))
